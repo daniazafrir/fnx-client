80860237888b7c3a8f75d1552b2e4eba607343ed
Daniaz <Dania@ideo-digital.com>
2024-01-31 13:56:27 +0200

    change folder name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { JwtInterceptor } from "./interceptors/jwt-interceptor";
 import { GalleryStore } from "./stores/gallery-store";
-import { HomeComponent } from './shared/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [		
